05762de08ad131a0ff693619223389a286f10a34
jessicafarias <jessica.farias.rosado@gmail.com>
2020-08-24 14:59:26 -0500

    Set up the Back-End Create model and controller for User Adding only new and create verbs for User Adding validations to each User params Adding new.html.erb for users Create and migrate db

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+    def new
+        #@User = .new
+    end
+
+    def create
+        # @User = Object.new(params[:User])
+        # if @User.save
+        #   flash[:success] = "Object successfully created"
+        #   redirect_to @User
+        # else
+        #   flash[:error] = "Something went wrong"
+        #   render 'new'
+        # end
+    end
+    
+    
+end
