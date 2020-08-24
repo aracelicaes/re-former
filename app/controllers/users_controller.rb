97831cf6e3aed3bfe9c38ce2ddaaa606ab2398c1
aracelicaes <aracelicaes@gmail.com>
2020-08-24 16:27:49 -0500

    Created HTML Form Added HTML form in new.html.erb Added Create method in user_controller Add private #user_params in controller Created db and migrated.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,23 @@
 class UsersController < ApplicationController
-    def new
-        #@User = .new
-    end
+  def new
+      #@User = .new
+  end
 
-    def create
-        # @User = Object.new(params[:User])
-        # if @User.save
-        #   flash[:success] = "Object successfully created"
-        #   redirect_to @User
-        # else
-        #   flash[:error] = "Something went wrong"
-        #   render 'new'
-        # end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+        flash[:success] = "Object successfully created"
+        redirect_to new_user_path
+    else
+        flash[:error] = "Something went wrong"
+        render 'new'
     end
-    
-    
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
 end
